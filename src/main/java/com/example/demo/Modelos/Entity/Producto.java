42a9a5f7a9624b2acd7f849c4443a95390144ef1
Santiago Galeano Rivera <santiago.galeano82242@elpoli.edu.co>
2026-09-16 18:50:50 -0500

    Nuevo diseño css

--- src/main/java/com/example/demo/Modelos/Entity/Producto.java
+++ src/main/java/com/example/demo/Modelos/Entity/Producto.java
@@ -1,8 +1,5 @@
 package com.example.demo.Modelos.Entity;
 
-import java.util.Date;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
